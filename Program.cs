468fdb2489bfe4c736b752ec32454c4ab15bd12b
hcrudge <hcrudge1@gmail.com>
2021-09-07 18:21:14 -0500

    phase 5 complete

--- Program.cs
+++ Program.cs
@@ -8,68 +8,69 @@ namespace plan_your_heist
     {
         static void Main(string[] args)
         {
-            
-            int bankDifficulty = 100;
-            int BankLuck = new Random().Next(-11,11);
-            int BankLuckDifficulty = bankDifficulty + BankLuck;
+
+            int bankDifficulty = 0;
+            int bankLuck = new Random().Next(-11, 11);
+            int TrialRuns = 1;
 
             Console.WriteLine("Plan your heist!");
-            
+            Console.WriteLine("How many trial runs would you like to make?");
+            TrialRuns = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Bank's difficulty level. 1-100");
+            bankDifficulty = int.Parse(Console.ReadLine());
+            int bankLuckDifficulty = bankLuck + bankDifficulty;
             Gang gang = new Gang("Delta");
-         
-            while (true)
+
+            for (int i = 0; i < TrialRuns; i++)
             {
-                Console.WriteLine("Please enter your name: ");
-                string name = Console.ReadLine();
-                if (name == "")
+                Heist();
+            }
+            void Heist()
+            {
+
+
+
+                while (true)
                 {
-                    break;
+                    Console.WriteLine("Please enter your name: ");
+                    string name = Console.ReadLine();
+                    if (name == "")
+                    {
+                        break;
+                    }
+
+                    Member newMember = new Member();
+                    newMember.Name = name;
+                    Console.WriteLine("Please enter a positive interger (1-10) of your skill level:");
+                    newMember.SkillLevel = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter a positive decimal between 0.0-2.0 for your courage level: ");
+                    newMember.Courage = double.Parse(Console.ReadLine());
+                    gang.Members.Add(newMember);
                 }
 
-                Member newMember = new Member();
-                newMember.Name = name;
-                Console.WriteLine("Please enter a positive interger (1-10) of your skill level:");
-                newMember.SkillLevel = int.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter a positive decimal between 0.0-2.0 for your courage level: ");
-                newMember.Courage = double.Parse(Console.ReadLine());
-                gang.Members.Add(newMember);
+                Console.WriteLine($"Total members in gang: {gang.Members.Count}");
+
+
+                foreach (Member member in gang.Members)
+                {
+                    gang.GangSkill += member.SkillLevel;
+                }
+                Console.WriteLine($"There are {gang.Members.Count} members in your gang with a total skill of {gang.GangSkill}.");
+                Console.WriteLine($"The gang's total skill is {gang.GangSkill}");
+                Console.WriteLine($"The Bank's state of the art security system is at level {bankLuckDifficulty}");
+
+                if (gang.GangSkill >= bankLuckDifficulty)
+                {
+                    Console.WriteLine("$$$$$ Success! Your gang's skill was enought to trump the banks state of the art system! $$$$$$$$");
+                }
+                else
+                {
+                    Console.WriteLine("Epic Fail! Your gang is not skilled enough for this bank's state of the art security system!");
+                }
+
             }
 
-            Console.WriteLine($"Total members in gang: {gang.Members.Count}");
 
-
-            foreach (Member member in gang.Members)
-            {
-                gang.GangSkill += member.SkillLevel;
-            }
-           Console.WriteLine($"There are {gang.Members.Count} members in your gang with a total skill of {gang.GangSkill}.");
-           Console.WriteLine($"The gang's total skill is {gang.GangSkill}");
-           Console.WriteLine($"The Bank's security system state of the art level is {BankLuckDifficulty}");
-
-            if (gang.GangSkill >= bankDifficulty)
-            {
-               Console.WriteLine("$$$$$ Success! Your gang's skill was enought to trump the banks state of the art system! $$$$$$$$"); 
-            }
-            else 
-            {
-                Console.WriteLine("Epic Fail! Your gang is not skilled enough for this bank's state of the art security system!");
-            }
-           
-
-
-            // int skill = member.SkillLevel.Count;
-
-            // int Sum=0;
-
-            // for (int i=0; i=skill.Count; i++)
-            // {
-            //     Sum+=skill[i];
-            // }
-
-            // foreach (Member member in gang.Members)
-            // {
-            //     Console.Write(gang.ListMembers);
-            // }
         }
 
 
